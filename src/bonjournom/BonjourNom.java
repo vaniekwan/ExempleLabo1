2e2c72cb36c595b729794b29a10d747c9fe2e282
vaniekwan <vaniekwan@localhost>
2021-05-25 14:44:13 -0400

    nom

--- src/bonjournom/BonjourNom.java
+++ src/bonjournom/BonjourNom.java
@@ -19,6 +19,8 @@ public class BonjourNom {
         Person John = new Person(args[0],args[1], 35);
 
         System.out.println("Bonjour"+ John.getFirstName() + " " + John.getLastName());
+        System.out.println("Bonjour"+ John.getFirstName() + " " + John.getLastName());
+             
         
     }
     
